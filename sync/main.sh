d1cf0f7219ff33bf814cc37c7c324bac474e3e06
lager1 <machv@cesnet.cz>
2018-06-13 12:14:32 +0200

    force configuration & data renewal once a day

--- sync/main.sh
+++ sync/main.sh
@@ -11,43 +11,50 @@
 # ===============================================================
 function generate_roles
 {
-
-  # TODO - volat pouze pri zmene
-  # TODO - diff ldap sync souboru?
-
   role_file="/etc/icingaweb2/roles.ini"
   role_base="/etc/icingaweb2/roles_base.ini"
   cp $role_base $role_file      # copy base
   ./roles.js                    # generate realm amdin roles
-
-  #admins=$(mysql -u icinga2 --password=$db_pass -e 'select uid from admin;' ldap_to_icinga | tail -n +2)
- 
-  #for i in $admins
-  #do
-  #  echo "admin: $i"
-  #done 
-
-
-
 }
 # ===============================================================
 # generate roles for users
 # ===============================================================
 function ldap_sync
 {
-  ./ldap_sync.js > /tmp/ldap_sync
+  if [[ -f /tmp/ldap_sync ]]
+  then
+    cp /tmp/ldap_sync /tmp/ldap_sync.old
+  fi
+  ./ldap_sync.js $1 > /tmp/ldap_sync
   mysql ldap_to_icinga < /tmp/ldap_sync
+
+  if [[ -f /tmp/ldap_sync && -f /tmp/ldap_sync.old ]]
+  then
+    diff -q /tmp/ldap_sync{,.old} >/dev/null
+    return $?
+  fi
+
+  return 1
 }
 # ===============================================================
 # main
 # ===============================================================
 function main
 {
-  # TODO - vynucene spusteni jednou denne
+  last=$(date -d "5 minutes ago" "+%Y-%m-%d")  # %Y-%m-%d 5 minutes ago
+  current=$(date "+%Y-%m-%d")
 
-  #source config/secrets.sh
-  #ldap_sync
-  generate_roles #- TODO - lepe v javascriptu?
+  if [[ "$last" != "$current" ]]               # force sync once a day
+  then
+    ldap_sync force
+    generate_roles
+  else
+    ldap_sync
+    if [[ $? -ne 0 ]]
+    then
+      generate_roles            # generate roles if ldap sync did something
+    fi
+  fi
 }
 # ===============================================================
 main

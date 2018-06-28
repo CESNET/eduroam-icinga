6df3dd9aee8f8a4c97ccea235deb052ef676a6b0
lager1 <machv@cesnet.cz>
2018-06-28 12:31:56 +0200

    add work in progress

--- sync/ldap_sync.js
+++ sync/ldap_sync.js
@@ -47,6 +47,7 @@ function synchronize_data() {
   var radius_servers = {};
   var admins = {};
   var testing_ids = {};
+  var disabled_realms = {};
 
   async.series([
     function(callback) {
@@ -72,10 +73,16 @@ function synchronize_data() {
       print_realms(realms, callback);
     },
     function(callback) {
-      search_radius_servers(client, radius_servers, config.search_base_radius, callback);
+      search_radius_servers(client, radius_servers, disabled_realms, config.search_base_radius, callback);
+    },
+    function(callback) {
+      delete_disabled(realms, disabled_realms, testing_ids, callback);
     },
     function(callback) {
       print_radius_servers(radius_servers, callback);
+    },
+    function(callback) {
+      print_services(radius_servers, realms, testing_ids, disabled_realms, callback);
     },
   ],
   // optional callback
@@ -209,7 +216,7 @@ function search_realms(client, data, testing_ids, search_base, callback)
     res.on('searchEntry', function(entry) {
       var key = entry.object['dn'].toLowerCase();
 
-      data[key.toLowerCase()] = entry.object;          // save whole object as normalized dn value
+      data[key] = entry.object;          // save whole object as normalized dn value
 
       if(entry.object['eduroamTestingId'])
         testing_ids[entry.object['eduroamTestingId']] = { "id" : entry.object['eduroamTestingId'], "password" : entry.object['eduroamTestingPassword'] };
@@ -275,15 +282,78 @@ function print_realms(data, callback)
   callback(null);
 }
 // --------------------------------------------------------------------------------------
+// TODO
+// --------------------------------------------------------------------------------------
+function set_disabled_realm(disabled_realms, realm, type)
+{
+  if(realm in disabled_realms) {
+    if(type in disabled_realms[realm])          // type exists
+      disabled_realms[realm][type]++;
+    else
+      disabled_realms[realm][type] = 1;         // key exists, but first of its type
+  }
+  else {
+    disabled_realms[realm] = {};
+    disabled_realms[realm][type] = 1;           // key does not exist
+  }
+}
+// --------------------------------------------------------------------------------------
+// TODO
+// --------------------------------------------------------------------------------------
+function store_radius_server(data, key, ldap_object, disabled_realms)
+{
+  var types = [ "eduroamMonRealm", "eduroamInfRealm" ];
+
+  // radius is disabled, delete realm
+  //                      // TODO - co kdyz nastane pripad, kdy by na jednom realmu byl zaroven aktivni a neaktivni server?
+  //                      // spravne by to melo dopadnout tak, ze pouze pokud pro dany realm budou vsechny servery neaktivni, tak bude vymazan z dalsiho zpracovani
+  if(ldap_object.radiusDisabled == 'true') {              // radius is disabled, delete realm
+    // process realm types in loop
+    for(var type in types) {
+      if(typeof(ldap_object[types[type]]) === 'object') {         // multiple mon/inf realms
+        for(var realm in ldap_object[types[type]])
+          set_disabled_realm(disabled_realms, ldap_object[types[type]][realm].toLowerCase(), "disabled");
+      }
+      else if(ldap_object[types[type]] == undefined)              // set realm to NULL if it is undefined
+        ;
+      else                                                                      // single mon/inf realm
+        set_disabled_realm(disabled_realms, ldap_object[types[type]].toLowerCase(), "disabled");
+    }
+  }
+  else {
+    data[key] = ldap_object;
+    data[key].dn = data[key].dn.toLowerCase();                 // normalize dn
+
+    // process realm types in loop
+    for(var type in types) {
+      if(typeof(data[key][types[type]]) === 'object') {         // multiple mon/inf realms
+        var tmp = [];
+        for(var realm in data[key][types[type]]) {
+          set_disabled_realm(disabled_realms, data[key][types[type]][realm].toLowerCase(), "enabled");
+          tmp.push(data[key][types[type]][realm].toLowerCase());
+        }
+        data[key][types[type]] = tmp;
+      }
+      else if(ldap_object[types[type]] == undefined)              // set realm to NULL if it is undefined
+        data[key][types[type]] = 'NULL';
+      else {                                                                      // single mon/inf realm
+        data[key][types[type]] = data[key][types[type]].toLowerCase();
+        set_disabled_realm(disabled_realms, data[key][types[type]].toLowerCase(), "enabled");
+      }
+    }
+  }
+}
+// --------------------------------------------------------------------------------------
 // search ldap for radius servers
 // --------------------------------------------------------------------------------------
-function search_radius_servers(client, data, search_base, done)
+function search_radius_servers(client, data, disabled_realms, search_base, done)
 {
   var opts = {
     //filter: '(&(objectClass=eduRoamRadius)(!(radiusDisabled=*)))',              // only active servers
-    filter: '(&(objectClass=eduRoamRadius)(!(radiusDisabled=*))(eduroamIcingaEnabled=true))',              // only active servers, temporarily check if icinga enabled flag is present
+    filter: '(objectClass=eduRoamRadius)',              // inactive servers are needed to filter realms!
+    //filter: '(&(objectClass=eduRoamRadius)(!(radiusDisabled=*))(eduroamIcingaEnabled=true))',              // only active servers, temporarily check if icinga enabled flag is present
     scope: 'sub',
-    attributes: [ 'cn', 'eduroamInfRadiusSecret1', 'eduroamInfTransport', 'eduroamMonRadiusSecret', 'eduroamMonRealm', 'manager', 'eduroamInfRealm' ]
+    attributes: [ 'cn', 'eduroamInfRadiusSecret1', 'eduroamInfTransport', 'eduroamMonRadiusSecret', 'eduroamMonRealm', 'manager', 'eduroamInfRealm', 'radiusDisabled' ]
   };
 
   client.search(search_base, opts, function(err, res) {
@@ -291,34 +361,7 @@ function search_radius_servers(client, data, search_base, done)
 
     res.on('searchEntry', function(entry) {
       var key = entry.object['dn'].toLowerCase();
-      data[key] = entry.object;
-      data[key].dn = data[key].dn.toLowerCase();                 // normalize dn
-
-      // mon realms
-      if(typeof(data[key].eduroamMonRealm) === 'object') {         // multiple mon realms
-        var tmp = [];
-        for(var realm in data[key].eduroamMonRealm)
-          tmp.push(data[key].eduroamMonRealm[realm].toLowerCase());
-
-        data[key].eduroamMonRealm = tmp;
-      }
-      else if(entry.object.eduroamMonRealm == undefined)              // set realm to NULL if it is undefined
-        data[key].eduroamMonRealm = 'NULL';
-      else                                                                        // single mon realm
-        data[key].eduroamMonRealm = data[key].eduroamMonRealm.toLowerCase();
-
-      // inf realms
-      if(typeof(data[key].eduroamInfRealm) === 'object') {         // multiple inf realms
-        var tmp = [];
-        for(var realm in data[key].eduroamInfRealm)
-          tmp.push(data[key].eduroamInfRealm[realm].toLowerCase());
-
-        data[key].eduroamInfRealm = tmp;
-      }
-      else if(entry.object.eduroamInfRealm == undefined)              // set realm to NULL if it is undefined
-        data[key].eduroamInfRealm = 'NULL';
-      else                                                                        // single inf realm
-        data[key].eduroamInfRealm = data[key].eduroamInfRealm.toLowerCase();
+      store_radius_server(data, key, entry.object, disabled_realms);
     });
 
     res.on('error', function(err) {
@@ -604,6 +647,211 @@ function print_testing_ids(data, callback)
   callback(null);
 }
 // --------------------------------------------------------------------------------------
+// TODO
+// --------------------------------------------------------------------------------------
+function delete_disabled(realms, disabled_realms, testing_ids, callback)
+{
+  for(var i in disabled_realms) {
+    // no radius servers enabled for realm i
+    if("disabled" in disabled_realms[i] && disabled_realms[i].disabled > 0 && !("enabled" in disabled_realms[i])) {
+      if(realms[i]) {     // realm is not disconnected
+        delete testing_ids[realms[i].eduroamTestingId];  // delete testing id
+        delete realms[i];       // delete realm
+      }
+    }
+  }
+  callback();
+}
+// --------------------------------------------------------------------------------------
+// TODO
+// --------------------------------------------------------------------------------------
+function prepare_service_output(visitors_realm, mon_realm, j, home_server, realm)
+{
+  var out;
+  out = "(";
+  // key
+  out += "'" + visitors_realm + "@" + mon_realm + "@" + j + "',";
+
+  // visitors realm
+  out += "'" + visitors_realm + "',";
+
+  // visited realm
+  out += "'" + mon_realm + "',";
+
+  // radius hostname on which the test is being run
+  out += "'" + j + "',";
+
+  // home server for visitor's realm
+  out += "'" + home_server + "',";
+
+  // checking home realm [bool]
+  if(visitors_realm == mon_realm)
+    out += " " + 1 + ", ";
+  else
+    out += " " + 0 + ", ";
+    //out += "'false', ";
+
+  // inf radius
+  // TODO
+
+  // testing id, password
+  out += "'" + realm.eduroamTestingId + "',";
+  out += "'" + realm.eduroamTestingPassword + "'";
+
+  out += "),";
+  return out;
+}
+// --------------------------------------------------------------------------------------
+// TODO
+// --------------------------------------------------------------------------------------
+function multiple_home_servers(realms_radius, primary_realm, mon_realm, j, realm)
+{
+  var out;
+
+  if(typeof(realms_radius[primary_realm]) === 'object') {         // multiple home servers for visitor's realm
+    var tmp = "";
+
+    for(var m in realms_radius[primary_realm])
+      tmp += realms_radius[primary_realm][m] + ','
+    out = prepare_service_output(primary_realm, mon_realm, j, tmp, realm);
+  }
+  else
+    out = prepare_service_output(primary_realm, mon_realm, j, realms_radius[primary_realm], realm);
+
+  return out;
+}
+// --------------------------------------------------------------------------------------
+// TODO
+// --------------------------------------------------------------------------------------
+function process_services(radius_servers, mon_realm, realms, realms_radius)
+{
+  var out;
+
+  for(var l in realms) {
+    if(realms[l].eduroamTestingId != undefined) {
+      // key
+      if(typeof(realms[l].cn) === 'object')
+        var primary_realm = realms[l].cn[0];
+      else
+        var primary_realm = realms[l].cn;
+
+      for(var j in mon_realm) {
+        if(typeof(mon_realm[j]) === 'object') {         // multiple mon realms
+          // debug
+          //console.log("multiple mon realms for " + j);
+          //console.log("multiple mon realms for " + j);
+
+          for(var k in mon_realm[j]) {
+            out = multiple_home_servers(realms_radius, primary_realm, mon_realm[j][k], j, realms[l]);
+            console.log(out);
+          }
+        }
+        else {
+          out = multiple_home_servers(realms_radius, primary_realm, mon_realm[j], j, realms[l]);
+          console.log(out);
+        }
+      }
+    }
+  }
+
+  // spise nez out tisknout
+  // out je potreba nasledne upravit, aby na poslednim radku nebyla posledni carka
+}
+// --------------------------------------------------------------------------------------
+// TODO
+// --------------------------------------------------------------------------------------
+function reverse_mapping(mon_realm, realms_radius)
+{
+  // generate reverse mapping, always one realm as key
+  for(var i in mon_realm) {
+    if(typeof(mon_realm[i]) === 'object')       // get only one realm as key
+      for(var j in mon_realm[i]) {
+        var key = mon_realm[i][j];
+
+        if(key in realms_radius) {   // key exists, convert to array and append to it
+          var tmp = realms_radius[key];
+          realms_radius[key] = [];
+          realms_radius[key].push(tmp);
+          realms_radius[key].push(i);
+        }
+        else
+          realms_radius[key] = i;
+      }
+    else {
+      var key = mon_realm[i];
+
+      if(key in realms_radius) {   // key exists, convert to array and append to it
+        var tmp = realms_radius[key];
+        realms_radius[key] = [];
+        realms_radius[key].push(tmp);
+        realms_radius[key].push(i);
+      }
+      else
+        realms_radius[key] = i;
+    }
+  }
+}
+// --------------------------------------------------------------------------------------
+// print testing ids
+// --------------------------------------------------------------------------------------
+function print_services(radius_servers, realms, testing_ids, disabled_realms, callback)
+{
+  var out;
+  var mon_realm = {};
+  var inf_realm = {};
+  var realms_radius = {};       // reverse mapping for better indexing
+
+  console.log("INSERT INTO service VALUES");             // insert
+
+  for(var i in radius_servers) {
+    if(radius_servers[i].eduroamMonRealm != 'NULL') {      // not undefined
+      if(typeof(radius_servers[i].eduroamMonRealm) === 'object')  // multiple mon realms
+        for(var realm in radius_servers[i].eduroamMonRealm) {
+          //console.log(typeof(realms[radius_servers[i].eduroamMonRealm[realm]].cn));
+          //console.log(realms[radius_servers[i].eduroamMonRealm[realm]].cn);
+
+          if(typeof(realms[radius_servers[i].eduroamMonRealm[realm]].cn) === 'object')
+            var value = realms[radius_servers[i].eduroamMonRealm[realm]].cn[0];         // use primary realm only
+          else
+            var value = realms[radius_servers[i].eduroamMonRealm[realm]].cn;
+
+
+          if(radius_servers[i].cn in mon_realm) // key exists, add
+            mon_realm[radius_servers[i].cn].push(value)
+          else {                                 // create key
+            mon_realm[radius_servers[i].cn] = [];
+            mon_realm[radius_servers[i].cn].push(value)
+          }
+        }
+      else {
+        if(typeof(realms[radius_servers[i].eduroamMonRealm].cn) === 'object')
+          var value = realms[radius_servers[i].eduroamMonRealm].cn[0];         // use primary realm only
+        else
+          var value = realms[radius_servers[i].eduroamMonRealm].cn;
+
+        mon_realm[radius_servers[i].cn] = value;
+      }
+    }
+  }
+
+  // generate reverse mapping, always one realm as key
+  reverse_mapping(mon_realm, realms_radius);
+
+
+  //console.log(mon_realm);
+  //console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+  //console.log(realms_radius);
+
+  //console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+  //console.log(testing_ids);
+  //console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+  //console.log(disabled_realms);
+
+  process_services(radius_servers, mon_realm, realms, realms_radius);
+
+  callback(null);
+}
+// --------------------------------------------------------------------------------------
 // create mysql database structure to store data
 // --------------------------------------------------------------------------------------
 function create_db_structure(callback)
@@ -613,11 +861,11 @@ function create_db_structure(callback)
   console.log("DROP TABLE IF EXISTS realm;");
   console.log("DROP TABLE IF EXISTS admin;");
   console.log("DROP TABLE IF EXISTS testing_id;");
+  console.log("DROP TABLE IF EXISTS service;");
 
   // max varchar size set to 191 - because of https://stackoverflow.com/questions/1814532/1071-specified-key-was-too-long-max-key-length-is-767-bytes
   // 191 * 4 = 764
 
-  //console.log("DROP TABLE IF EXISTS realm_radius;");
   //console.log("DROP TABLE IF EXISTS radius_admin;");
   //console.log("DROP TABLE IF EXISTS radius_testing_id;");
 
@@ -631,6 +879,8 @@ function create_db_structure(callback)
 
   console.log("CREATE TABLE IF NOT EXISTS radius_server (id INT NOT NULL AUTO_INCREMENT, radius_dn VARCHAR(191) NOT NULL, radius_cn VARCHAR(191) NOT NULL, inf_radius_secret VARCHAR(191) NOT NULL, transport VARCHAR(191) NOT NULL, mon_radius_secret VARCHAR(191) NOT NULL, mon_realm VARCHAR(191), FOREIGN KEY (mon_realm) REFERENCES realm(realm_dn), inf_realm VARCHAR(191), FOREIGN KEY (inf_realm) REFERENCES realm(realm_dn), radius_manager VARCHAR(191) NOT NULL, FOREIGN KEY (radius_manager) REFERENCES admin(admin_dn), PRIMARY KEY ( id ), UNIQUE ( id ), INDEX radius_server_idx (radius_dn));");
 
+
+  console.log("CREATE TABLE IF NOT EXISTS service (id VARCHAR(191) NOT NULL, visitors_realm VARCHAR(191) NOT NULL, visited_realm VARCHAR(191) NOT NULL, check_host VARCHAR(191) NOT NULL, home_server VARCHAR(191) NOT NULL, home_realm_check BOOLEAN, testing_id VARCHAR(191) NOT NULL, password VARCHAR(191) NOT NULL, PRIMARY KEY ( id ), UNIQUE ( id ), INDEX service_idx (id));");
  
   //console.log("CREATE TABLE IF NOT EXISTS radius_server (id INT NOT NULL AUTO_INCREMENT, radius_dn VARCHAR(191) NOT NULL, radius_cn VARCHAR(191) NOT NULL, inf_radius_secret VARCHAR(191) NOT NULL, transport VARCHAR(191) NOT NULL, mon_radius_secret VARCHAR(191) NOT NULL, mon_realm VARCHAR(191), FOREIGN KEY (mon_realm) REFERENCES realm(realm_dn), inf_realm VARCHAR(191), FOREIGN KEY (inf_realm) REFERENCES realm(realm_dn), radius_manager VARCHAR(191) NOT NULL, FOREIGN KEY (radius_manager) REFERENCES admin(admin_dn), PRIMARY KEY ( id ), UNIQUE ( id ), INDEX radius_server_idx (radius_dn));");
 

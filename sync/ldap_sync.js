8482af7f0f6f6277c261e910e5161b6972b54833
lager1 <machv@cesnet.cz>
2018-07-04 09:45:54 +0200

    add institution.xml url to realms table

--- sync/ldap_sync.js
+++ sync/ldap_sync.js
@@ -252,9 +252,9 @@ function print_realms(data, callback)
     if(typeof(data[item].manager) === 'object') {  // multiple managers
       for(var j in data[item].manager) {
         if(data[item].eduroamTestingId)
-          out = "(" + num + ", '" + data[item].dn + "', '" + data[item].cn + "', '" + data[item].eduroamConnectionStatus + "', '" + data[item].eduroamMemberType + "', '" + data[item].manager[j] + "', '" + data[item].eduroamTestingId + "')";
+          out = "(" + num + ", '" + data[item].dn + "', '" + data[item].cn + "', '" + data[item].eduroamConnectionStatus + "', '" + data[item].eduroamMemberType +  "', '" + data[item].labeledUri + "', '" + data[item].manager[j] + "', '" + data[item].eduroamTestingId + "')";
         else
-          out = "(" + num + ", '" + data[item].dn + "', '" + data[item].cn + "', '" + data[item].eduroamConnectionStatus + "', '" + data[item].eduroamMemberType + "', '" + data[item].manager[j] + "', NULL)";        // save as NULL
+          out = "(" + num + ", '" + data[item].dn + "', '" + data[item].cn + "', '" + data[item].eduroamConnectionStatus + "', '" + data[item].eduroamMemberType + "', '" + data[item].labeledUri + "', '" + data[item].manager[j] + "', NULL)";        // save as NULL
 
 
         if(Object.keys(data).indexOf(item) == Object.keys(data).length - 1 && j == data[item].manager.length - 1) // last item and last manager
@@ -268,9 +268,9 @@ function print_realms(data, callback)
     }
     else {              // one manager only
       if(data[item].eduroamTestingId)
-        out = "(" + num + ", '" + data[item].dn + "', '" + data[item].cn + "', '" + data[item].eduroamConnectionStatus + "', '" + data[item].eduroamMemberType + "', '" + data[item].manager + "', '" + data[item].eduroamTestingId + "')";
+        out = "(" + num + ", '" + data[item].dn + "', '" + data[item].cn + "', '" + data[item].eduroamConnectionStatus + "', '" + data[item].eduroamMemberType + "', '"  + data[item].labeledUri + "', '" + data[item].manager + "', '" + data[item].eduroamTestingId + "')";
       else
-        out = "(" + num + ", '" + data[item].dn + "', '" + data[item].cn + "', '" + data[item].eduroamConnectionStatus + "', '" + data[item].eduroamMemberType + "', '" + data[item].manager + "', NULL)";
+        out = "(" + num + ", '" + data[item].dn + "', '" + data[item].cn + "', '" + data[item].eduroamConnectionStatus + "', '" + data[item].eduroamMemberType + "', '"  + data[item].labeledUri + "', '" + data[item].manager + "', NULL)";
 
       if(Object.keys(data).indexOf(item) == Object.keys(data).length - 1) // last item
         out += ";";
@@ -874,7 +874,7 @@ function create_db_structure(callback)
 
   console.log("CREATE TABLE IF NOT EXISTS testing_id (id VARCHAR(191) NOT NULL, password VARCHAR(191) NOT NULL, INDEX testing_id_idx(id));");
 
-  console.log("CREATE TABLE IF NOT EXISTS realm (id INT NOT NULL AUTO_INCREMENT, realm_dn VARCHAR(191) NOT NULL, realm_cn VARCHAR(191) NOT NULL, status VARCHAR(191) NOT NULL, member_type VARCHAR(191) NOT NULL, realm_manager VARCHAR(191) NOT NULL, FOREIGN KEY (realm_manager) REFERENCES admin(admin_dn), testing_id VARCHAR(191), FOREIGN KEY (testing_id) REFERENCES testing_id(id), PRIMARY KEY ( id ), UNIQUE ( id ), INDEX realm_idx (realm_dn));");
+  console.log("CREATE TABLE IF NOT EXISTS realm (id INT NOT NULL AUTO_INCREMENT, realm_dn VARCHAR(191) NOT NULL, realm_cn VARCHAR(191) NOT NULL, status VARCHAR(191) NOT NULL, member_type VARCHAR(191) NOT NULL, xml_url VARCHAR(191) NOT NULL, realm_manager VARCHAR(191) NOT NULL, FOREIGN KEY (realm_manager) REFERENCES admin(admin_dn), testing_id VARCHAR(191), FOREIGN KEY (testing_id) REFERENCES testing_id(id), PRIMARY KEY ( id ), UNIQUE ( id ), INDEX realm_idx (realm_dn));");
 
   console.log("CREATE TABLE IF NOT EXISTS radius_server (id INT NOT NULL AUTO_INCREMENT, radius_dn VARCHAR(191) NOT NULL, radius_cn VARCHAR(191) NOT NULL, inf_radius_secret VARCHAR(191) NOT NULL, transport VARCHAR(191) NOT NULL, mon_radius_secret VARCHAR(191) NOT NULL, mon_realm VARCHAR(191), FOREIGN KEY (mon_realm) REFERENCES realm(realm_dn), inf_realm VARCHAR(191), FOREIGN KEY (inf_realm) REFERENCES realm(realm_dn), radius_manager VARCHAR(191) NOT NULL, FOREIGN KEY (radius_manager) REFERENCES admin(admin_dn), PRIMARY KEY ( id ), UNIQUE ( id ), INDEX radius_server_idx (radius_dn));");
 

074fe0a7ecdb3b67c1207d84419fb8321bd3051d
lager1 <machv@cesnet.cz>
2018-07-18 15:03:28 +0200

    implement main synchronization part

--- sync/main.sh
+++ sync/main.sh
@@ -7,6 +7,78 @@
 
 
 # ===============================================================
+# notify admins about sync problem
+# ===============================================================
+function notify
+{
+  echo -e "$1" | mail -s "icinga2 sync problem" $admin
+}
+# ===============================================================
+# deploy new configuration
+# ===============================================================
+function deploy_config
+{
+  # deploy
+  out=$(icingacli director config deploy)
+
+  if [[ "$out" =~ "nothing to do" ]]
+  then
+    exit 0
+  fi
+
+  # wait for deploy to finish
+  sleep 30
+
+  # get deployed config state
+  status=$(mysql -u $db_user -e 'select id,startup_succeeded from director_deployment_log order by id desc limit 1;' --password="$db_pass" director)
+
+  id=$(echo "$status" | tail -1 | awk '{ print $1 }')
+  success=$(echo "$status" | tail -1 | awk '{ print $2 }')
+
+  deploy=$(mysql -u $db_user -e "select startup_log from director_deployment_log where id=$id;" --password="$db_pass" director)
+
+  if [[ "$success" != "y" ]]
+  then
+    notify "Failed deploy:\n$deploy"
+    exit 1
+  fi
+
+  if [[ $(echo "$deploy" | grep "warning") != "" ]]
+  then
+    notify "configuration warnings:\n$deploy"
+  fi
+}
+# ===============================================================
+# sync director import sources & sync rules
+# ===============================================================
+function sync_director
+{
+  sources=$(icingacli director importsource list | grep '[[:digit:]]' | cut -d "|" -f 1 | tr -d " ")
+
+  # import sources
+  for i in $sources
+  do
+    state=$(icingacli director importsource check --id $i)
+
+    if [[ $(echo "$state" | grep "This Import Source failed:") != "" ]]
+    then
+      notify "Failed import source:\n$state"
+      exit 1
+    elif [[ $(echo "$state" | grep "pending changes") != "" ]]
+    then
+     icingacli director importsource run --id $i
+     # TODO - return value handling?
+    fi
+  done
+
+  # sync rules
+  for i in $sync_rules
+  do
+     icingacli director syncrule run --id $i &>/dev/null
+     # TODO - return value handling?
+  done
+}
+# ===============================================================
 # generate synchronize ldap source with mysql
 # ===============================================================
 function ldap_sync
@@ -27,6 +99,14 @@ function ldap_sync
   return 1
 }
 # ===============================================================
+# synchronize data
+# ===============================================================
+function sync_data
+{
+  sync_director
+  deploy_config
+}
+# ===============================================================
 # main
 # ===============================================================
 function main
@@ -37,15 +117,19 @@ function main
   if [[ "$last" != "$current" ]]               # force sync once a day
   then
     ldap_sync force
-    : # TODO
+    sync_data
   else
     ldap_sync
     if [[ $? -ne 0 ]]
     then
-      : # TODO
+      sync_data
     fi
   fi
 }
+# ===============================================================
+# config
+source config/config.sh
+source config/secrets.sh
 # ===============================================================
 main
 

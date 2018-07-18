77619589367bf26020b889adddb00de8cb3bb1a2
lager1 <machv@cesnet.cz>
2018-07-18 15:08:51 +0200

    add sync config

---
+++ sync/config/config.sh
@@ -0,0 +1,8 @@
+# order of sync rules
+# get this list by 'icingacli director syncrule list'
+# the order here is important
+# users depends on synced user groups
+# hosts depeneds on synced host groups
+sync_rules="15 14 16 17 19"
+# admin mail
+admin="machv@cesnet.cz"

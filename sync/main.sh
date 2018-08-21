5b4f05354e76f6758e3c9d80fb1a957eb7176f35
lager <machv@cesnet.cz>
2018-08-21 14:28:02 +0200

    fix database access

--- sync/main.sh
+++ sync/main.sh
@@ -88,7 +88,7 @@ function ldap_sync
     cp /tmp/ldap_sync /tmp/ldap_sync.old
   fi
   ./ldap_sync.js $1 > /tmp/ldap_sync
-  out=$(mysql ldap_to_icinga < /tmp/ldap_sync 2>&1)
+  out=$(mysql -u $icinga_db_user --password="$icinga_db_pass" ldap_to_icinga < /tmp/ldap_sync 2>&1)
   ret=$?
 
   if [[ $ret -ne 0 ]]

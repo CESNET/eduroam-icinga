04bf51e7e4fb718d1c8a22f2f53b45ef45cf820a
lager <machv@cesnet.cz>
2018-08-31 13:17:11 +0200

    update cleanup script

--- other/cleanup.sh
+++ other/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cleanup temporary files left by rad_eap_test when icinga2 is reloaded
-for i in $(find /tmp/ -name 'rad*' -atime +5); do rm -r $i; done
+for i in $(find /tmp/ -maxdepth 0 -name 'rad*' -atime +5); do rm -r $i; done

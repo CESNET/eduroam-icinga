8feebdfc4650716675bfc715e0125e831c9da025
lager <machv@cesnet.cz>
2018-09-06 14:08:19 +0200

    use amin instead of atime

--- other/cleanup.sh
+++ other/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cleanup temporary files left by rad_eap_test when icinga2 is reloaded
-for i in $(find /tmp/ -maxdepth 1 -name 'rad*' -atime +5); do rm -r $i; done
+for i in $(find /tmp/ -maxdepth 1 -name 'rad*' -amin +10); do rm -r $i; done

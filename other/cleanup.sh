8bc5e592428aac30d53e26a90bfab9c1599a6db5
lager <machv@cesnet.cz>
2019-02-05 12:05:02 +0100

    check if directory exists before removing it

--- other/cleanup.sh
+++ other/cleanup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # cleanup temporary files left by rad_eap_test when icinga2 is reloaded
-for i in $(find /tmp/ -maxdepth 1 -name 'rad*' -amin +10); do rm -r $i; done
+for i in $(find /tmp/ -maxdepth 1 -name 'rad*' -amin +10)
+do
+  if [[ -d "$i" ]]
+  then
+    rm -r $i
+  fi
+done
 
 # cleanup sync files left by synchronization script for debug purposes
 for i in $(find /var/lib/nagios/sync_logs/* -atime +30); do rm $i; done

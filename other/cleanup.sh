4b05162b739969f7889bbc11a8537619f7a79108
lager <machv@cesnet.cz>
2019-02-10 14:41:36 +0100

    ignore find errors

--- other/cleanup.sh
+++ other/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # cleanup temporary files left by rad_eap_test when icinga2 is reloaded
-for i in $(find /tmp/ -maxdepth 1 -name 'rad*' -amin +10)
+for i in $(find /tmp/ -maxdepth 1 -name 'rad*' -amin +10 2>/dev/null)
 do
   if [[ -d "$i" ]]
   then

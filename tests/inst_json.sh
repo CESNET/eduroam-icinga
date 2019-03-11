c0a68f638c20b4472f31b992c1658194f0634f10
lager <machv@cesnet.cz>
2019-03-11 16:30:08 +0100

    fix URL

--- tests/inst_json.sh
+++ tests/inst_json.sh
@@ -4,7 +4,7 @@
 # ========================================================================
 function main
 {
-  out=$(wget -4 -q -O - https://monitor.eduroam.org/eduroam-database/v2/scripts/json_validation_test.php?url=https://monitor.eduroam.cz/json/$2/institution.json 2>/dev/null)
+  out=$(wget -4 -q -O - https://monitor.eduroam.org/eduroam-database/v2/scripts/json_validation_test.php?url=https://www.eduroam.cz/general/institution.json 2>/dev/null)
 
   # check validation output
   if [[ "$out" == "JSON validates OK" ]]

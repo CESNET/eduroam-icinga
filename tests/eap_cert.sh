bed4b8bb229638ab8da07ecca1801b6b5ccd4c2e
lager <machv@cesnet.cz>
2019-05-22 23:10:27 +0200

    rewrite most of the test, use git repo for certs

--- tests/eap_cert.sh
+++ tests/eap_cert.sh
@@ -9,32 +9,21 @@
 # so all the necesarry params for rad_eap_test are required
 # ==============================================================================
 
-
 # ==============================================================================
 # main function
 # ==============================================================================
 function main()
 {
-  out=$($plugin_path/eduroam_cat.sh -p $1 | tail -1)
-  ret=$?
-  realm=$1      # save realm
+  realm=$1                  # save realm
+  shift                     # shift params
 
-  shift         # shift params
-
-  if [[ $ret -eq 0 ]]       # found
+  # check if realm_eap_config.xml exists
+  if [[ -e "$cat_db/${realm}_eap_config.xml" ]]     # if it exists, then the institution is present in CAT
   then
-    parse_eap_config $out
-    run_rad_eap_test "$@"
-  elif [[ $ret -eq 1 ]]
-  then
-    # TODO
-    :
-  elif [[ $ret -eq 2 ]]     # NOT found
-  then
-    run_rad_eap_test "$@"
-  else  # UNKNOWN
-    # TODO
-    :
+    parse_eap_config
+    run_rad_eap_test "$@" -x "$hostname" -a "$db/${realm}_chain.pem"   # run rad_eap_test, check cert against CA and EAP server certificate subject
+  else
+    run_rad_eap_test "$@"     # run rad_eap_test
   fi
 }
 # ==============================================================================
@@ -42,85 +31,174 @@ function main()
 # ==============================================================================
 function analyze_cert()
 {
-  if [[ -n "$chain" && -n "$hostname" ]]
-  then
-    :
-    # check cert against CAT info
-  fi
-
   #do regular cert checks
   cert_info=$(openssl x509 -nameopt utf8 -in $1 -text -noout)
   echo "$cert_info"
 
 
-  # TODO - drzet certy (jak chainy, tak eap) v gitu?
+  # TODO
 }
 # ==============================================================================
 # run rad_eap_test
 # ==============================================================================
 function run_rad_eap_test()
 {
+  local out
+  local ret
+
   cert=$(mktemp)
-  echo "cert: $cert"
 
-  $plugin_path/rad_eap_test "$@" -B $cert >/dev/null   # dont need this output
+  out=$($plugin_path/rad_eap_test "$@" -B $cert)   # dont need this output
+  ret=$?
 
-  # TODO - check return code of rad_eap_test?
-
-  if [[ ! -e $db/${realm}_eap.pem ]]    # eap cert does not exist
+  if [[ $ret -ne 0 ]]       # if rad_eap_test returned any error, display it and exit immediately
   then
-     write_cert $realm "eap"
-  elif [[ "$(diff -q $cert $db/${realm}_eap.pem)" != "" ]]    # cert differs from current cert
-  then
-    # TODO - notify admins ?
-     write_cert $realm "eap"
+    echo "$out"
+    exit $ret
   fi
 
-  rm $cert
+  # eapol_test extracts certs in strange format, we want to use the standard format
+  # this extract just the first certificate in standard format
+  sed -n -i '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' $cert
+
+  if [[ ! -e "$db/${realm}_eap.pem" ]]    # eap cert does not exist
+  then
+    write_cert $realm "$(cat $cert)"
+    commit_changes "added EAP certificate for $realm" "${realm}_eap.pem"
+
+  elif [[ "$(diff -q $cert $db/${realm}_eap.pem)" != "" ]]    # cert differs from current cert
+  then
+    write_cert $realm "$(cat $cert)"
+    commit_changes "changed EAP certificate for $realm" "${realm}_eap.pem"
+  fi
+
+  rm $cert      # remove temp file
+
+  check_cert_changes "$db/${realm}_eap.pem"     # check when the file was last added/changed
+
+  if [[ $? -ne 0 ]]
+  then
+    echo "WARNING: RADIUS server EAP certificate changed recently"
+    exit 2
+  fi
+
   analyze_cert $db/${realm}_eap.pem
+}
+# ==============================================================================
+# get last modification time of specified file from git repostitory in seconds
+# if the specified file has only 1 commit, no changes are assumed
+# params:
+# 1) file
+# ==============================================================================
+function get_last_modify_time()
+{
+  count=$(git rev-list --count master "$1")
+
+  if [[ $count -eq 1 ]]
+  then
+    return 0
+  fi
+
+  last_modify_time=$(date -d $(git log -1 --format=%cd --date=iso-strict "$1") "+%s")       # get last modify time from git from speficic file
+}
+# ==============================================================================
+# check changes in certificates based on git repository
+# params:
+# 1) file to check
+# ==============================================================================
+function check_cert_changes()
+{
+  local curr_date=$(date "+%s")
+  local time_since_last_change=259200       # 3 days in seconds
+
+  get_last_modify_time "$1"
+
+  if [[ $(($last_modify_time + $time_since_last_change)) -ge $curr_date ]]
+  then
+    return 1        # the file changed recently
+  else
+    return 0        # no changes
+  fi
 }
 # ==============================================================================
 # write given cert to "db"
 # params:
 # 1) realm
-# 2) cert type
+# 2) cert content
 # ==============================================================================
 function write_cert()
 {
-  echo "-----BEGIN CERTIFICATE-----" > "$db/${1}_${2}.pem"
-  echo $chain >> "$db/${1}_${2}.pem"
-  echo "-----END CERTIFICATE-----" >> "$db/${1}_${2}.pem"
+  echo "$2" > "$db/${1}_eap.pem"
+}
+# ==============================================================================
+# write given chain to "db"
+# params:
+# 1) realm
+# ==============================================================================
+function write_chain()
+{
+  echo "-----BEGIN CERTIFICATE-----" > "$db/${1}_chain.pem"
+  echo $chain >> "$db/${1}_chain.pem"
+  echo "-----END CERTIFICATE-----" >> "$db/${1}_chain.pem"
 }
 # ==============================================================================
 # parse eap config
-# params:
-# 1) realm
-# 2) profile id
 # ==============================================================================
 function parse_eap_config()
 {
   # read CA chain from xml
-  chain=$(echo -n "$db/${realm}_${1}_eap_config.xml" | python3 -c 'import sys; import lxml.objectify; f = sys.stdin.read(); data = lxml.objectify.parse(f).getroot(); print(data.EAPIdentityProvider.AuthenticationMethods.AuthenticationMethod.ServerSideCredential.CA)')
+  chain=$(echo -n "$cat_db/${realm}_eap_config.xml" | python3 -c 'import sys; import lxml.objectify; f = sys.stdin.read(); data = lxml.objectify.parse(f).getroot(); print(data.EAPIdentityProvider.AuthenticationMethods.AuthenticationMethod.ServerSideCredential.CA)')
 
   # read server hostname from xml
-  hostname=$(echo -n "$db/${realm}_${1}_eap_config.xml" | python3 -c 'import sys; import lxml.objectify; f = sys.stdin.read(); data = lxml.objectify.parse(f).getroot(); print(data.EAPIdentityProvider.AuthenticationMethods.AuthenticationMethod.ServerSideCredential.ServerID)')
+  hostname=$(echo -n "$cat_db/${realm}_eap_config.xml" | python3 -c 'import sys; import lxml.objectify; f = sys.stdin.read(); data = lxml.objectify.parse(f).getroot(); print(data.EAPIdentityProvider.AuthenticationMethods.AuthenticationMethod.ServerSideCredential.ServerID)')
 
   # no cert exists
   if [[ ! -e "$db/${realm}_chain.pem" ]]
   then
-    write_cert $1 "chain"
+    write_chain $realm
+    commit_changes "added certificate chain from CAT for $realm" "${realm}_chain.pem"
 
-  # rewrite the cert only if it changed
-  # TODO - some notification?
   elif [[ $(diff -q <(echo "-----BEGIN CERTIFICATE-----" ; echo $chain ; echo "-----END CERTIFICATE-----" ;) "$db/${realm}_chain.pem") != "" ]]
   then
-    write_cert $1 "chain"
+    write_chain $realm
+    commit_changes "changed certificate chain from CAT for $realm" "${realm}_chain.pem"
   fi
 
+  check_cert_changes "$db/${realm}_chain.pem"   # check when the file was last added/changed
+
+  if [[ $? -ne 0 ]]
+  then
+    echo "WARNING: CA certificate from CAT changed recently"
+    exit 2
+  fi
+}
+# ======================================================================
+# commit all changes
+# params:
+# 1) commit message
+# 2) file to add
+# ======================================================================
+function commit_changes()
+{
+  cd $db
+  git add "$2"
+
+  count=0
+  while [[ -e ".git/index.lock" && count -lt 30 ]]      # wait for other git processes to finish
+  do
+    sleep 1
+    ((count++))
+  done
+
+  git commit -m "$1" --author "info@eduroam.cz <info@eduroam.cz>" &>/dev/null
+  cd - &>/dev/null
 }
 # ==============================================================================
 db="/var/lib/nagios/eap_cert_db"
+cat_db="/var/lib/nagios/CAT_db/"
 plugin_path="/usr/lib/nagios/plugins"
 # ==============================================================================
+# ==============================================================================
 main "$@"
+# ==============================================================================
 
